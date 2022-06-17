5158fecf36700762f0311d9ac923d7cbc993d9b3
Jack Wheeler <jack.wheeler@rogers.com>
2022-06-16 20:03:39 -0400

    Completely redid the the rule system and changed how the replace, and context search works.

--- Lsystem.py
+++ Lsystem.py
@@ -2,27 +2,27 @@ from turtle import *
 from random import *
 from math import *
 
-def create_string(axiom, generator, order):
-    newFractal = axiom 
-    for j in range(order):
-        tempFractal = ""
-        for i in range(len(newFractal)):
-            found = False
-            for rule in generator:
-                if newFractal[i] == rule[0]:
-                    contextL = rule[1][1]
-                    contextR = rule[1][2]
-                    if (contextL == context_search(newFractal, i)[0] or contextL == "*"):
-                        if (contextR == context_search(newFractal, i)[1] or contextR == "*"):
-                            found = True
-                            tempFractal += rule[1][0]
-                            
-            if found == False:
-                tempFractal += newFractal[i]
-        newFractal = tempFractal
+class rule:
+    def __init__(self, seed, sprout, left, right):
+        self.seed = seed
+        self.sprout = sprout
+        self.left = left
+        self.right = right
+
+def replaceString(axiom, generator):
+    newFractal = ""
+    for i in range(len(axiom)):
+        for rule in generator:
+            replaced = False
+            if axiom[i] == rule.seed and (rule.left == contextSearch(axiom, i)[0] or rule.left == "*") and (rule.right == contextSearch(axiom, i)[1] or rule.right == "*"):
+                newFractal += rule.sprout
+                replaced = True
+                break
+        if replaced != True:
+            newFractal += axiom[i]
     return newFractal
 
-def context_search(tempString, pos):
+def contextSearch(tempString, pos):
     realContextL, realContextR = None, None
     ignore = ["F", "+", "-", "[", "]"]
     r, l = pos, pos
@@ -51,7 +51,7 @@ def context_search(tempString, pos):
         
     return [realContextL, realContextR]
 
-def draw_fractal(fractal, D, angle):
+def drawFractal(fractal, D, angle):
     positions = []
     for i in range(len(fractal)):
         if fractal[i] == 'F':
@@ -73,13 +73,4 @@ def draw_fractal(fractal, D, angle):
             seth(turtleState[1])
             D = turtleState[2]
             pd()
-            
-def random_fractals(axiom, generator, maxOrder, D, angle, amount):
-    for z in range(amount):
-        new_fractal = create_string(axiom, generator, round(random() * maxOrder))
-        pu()
-        sety(random() * 700 - 350)
-        setx(random() * 900 - 450)
-        seth(90)
-        pd()    
-        draw_fractal(new_fractal, D, angle)
+    done()
--- main.py
+++ main.py
@@ -1,34 +1,32 @@
 from math import *
 from Lsystem import *
 
-
-screensize(1000,800)
-speed(0)
-
-
-left(90)
-pu()
-sety(-350)
-setx(0)
-pd()
-
+def setupTurtle():
+    screensize(1000,800)
+    speed(0)
+    left(90)
+    pu()
+    sety(-350)
+    setx(0)
+    pd()
 
 angle = 16
 distance = 10
 axiom = "F1F1F1"
 
-generator = [["0", ["0", "0", "0"]],
-             ["0", ["1[+F1F1]", "0", "1"]],
-             ["1", ["1", "0", "0"]],
-             ["1", ["1", "0", "1"]],
-             ["0", ["0", "1", "0"]],
-             ["0", ["1F1", "1", "1"]],
-             ["1", ["0", "1", "0"]],
-             ["1", ["0", "1", "1"]],
-             ["+", ["-", "*", "*"]],
-             ["-", ["+", "*", "*"]]]
+genome =   [rule('0', '0', '0', '0'),
+            rule('0', '1[+F1F1]', '0', '1'),
+            rule('1', '1', '0', '0'),
+            rule('1', '1', '0', '1'),
+            rule('0', '0', '1', '0'),
+            rule('0', '1F1', '1', '1'),
+            rule('1', '0', '1', '0'),
+            rule('1', '0', '1', '1'),
+            rule('+', '-', '*', '*'),
+            rule('-', '+', '*', '*')]
 
-new_fractal = create_string(axiom, generator, 20)
-draw_fractal(new_fractal, distance, angle)
-
-done()
+new_fractal = axiom
+for i in range(14):
+    new_fractal = replaceString(new_fractal, genome)
+setupTurtle()
+drawFractal(new_fractal, distance, angle)
